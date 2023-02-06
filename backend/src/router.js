1919cc080577939eb7593426258aaea07c8da541
SIMON BULLADO <s_bullado51@hotmail.com>
2023-02-06 02:30:21 +0100

    Refactoring backend, jwttoken

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,19 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
-const MealControllers = require("./controllers/mealControllers");
+const MealsControllers = require("./controllers/MealsControllers");
+const UsersControllers = require("./controllers/UsersControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("/meals", MealsControllers.browse);
+router.get("/meals/:id", MealsControllers.read);
+router.put("/meals/:id", MealsControllers.edit);
+router.post("/meals", MealsControllers.add);
+router.delete("/meals/:id", MealsControllers.destroy);
 
-router.get("/meals", MealControllers.browse);
-router.get("/meals/:id", MealControllers.read);
-router.put("/meals/:id", MealControllers.edit);
-router.post("/meals", MealControllers.add);
-router.delete("/meals/:id", MealControllers.destroy);
+router.get("/users", UsersControllers.browse);
+router.get("/users/:id", UsersControllers.read);
+router.put("/users/:id", UsersControllers.edit);
+router.post("/users", UsersControllers.add);
+router.delete("/users/:id", UsersControllers.destroy);
 
 module.exports = router;
