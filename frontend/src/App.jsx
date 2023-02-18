d2d2c2b96c6be8eb4cba58854e2c84eb68385590
SIMON BULLADO <s_bullado51@hotmail.com>
2023-02-18 08:22:05 +0100

    Refactoring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
-import React from "react";
-import Home from "./pages/Home";
 import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return "hello";
 }
 
 export default App;
